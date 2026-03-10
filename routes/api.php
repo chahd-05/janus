9df49087ed0f5598ac35c0b4682f3b1063bda7fc
shahd <mazouzchahd12@gmail.com>
2026-03-10 14:15:34 +0000

    feat: create controllers route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\api\AutController;
 use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\HabitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/category', CategoryController::class);
+Route::apiResource('/habit', HabitController::class)->middleware('auth:sanctum');
+Route::post('/register', [AutController::class, 'register']);
+Route::post('/logout', [AutController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AutController::class, 'login']);
