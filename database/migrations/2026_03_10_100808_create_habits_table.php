998f75df7a0513b80d539c0a954ec94f5a42ef29
shahd <mazouzchahd12@gmail.com>
2026-03-10 10:36:26 +0000

    feat: create habits table

---
+++ database/migrations/2026_03_10_100808_create_habits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('User')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('frequency', ['daily', 'weekly', 'monthly']);
+            $table->unsignedInteger('target_day');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habits');
+    }
+};
