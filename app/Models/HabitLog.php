b5fd594c7a2f2796bef79857a7d9bd5a662cc7b8
shahd <mazouzchahd12@gmail.com>
2026-03-11 09:57:17 +0000

    feat: create HabitLog model with fillable attributes

---
+++ app/Models/HabitLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HabitLog extends Model
+{
+    protected $fillable = [
+        'note',
+        'date'
+    ];
+}
